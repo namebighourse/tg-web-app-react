91c2c364f89c8d5790b9c29c07e3c85d3e62d5b7
Mastersk1x <69405865+namebighourse@users.noreply.github.com>
2025-01-15 16:47:58 +0600

    feature: create app

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import './App.css';
+import './component/fonts/fonts.css'
+import 'animate.css'; 
+
+function App() {
+  return (
+    <div className="App">
+      <div className='container'>
+      <h1 className='animate__animatedanimate__start-text '>
+        Привет!
+      </h1>
+      <button className='button-quest'>
+        Познакомиться
+      </button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
