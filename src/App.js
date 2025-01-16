062ef89b6d32c6aebf02fa8a912300a8e85ab807
Mastersk1x <69405865+namebighourse@users.noreply.github.com>
2025-01-16 22:28:02 +0600

    feature: add animated coursor

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import './component/fonts/fonts.css'
 import 'animate.css'; 
+import AnimatedCursor from "react-animated-cursor"
 
 function App() {
   return (
     <div className="App">
+    <AnimatedCursor />
       <div className='container'>
       <h1 className='animate__animatedanimate__start-text '>
         Привет!
